e19b548c48f76f58f8f5ff54f94247b71d205494
Doenyas <doenyas@gmail.com>
2021-06-12 16:28:58 -0700

    Update sign.sh
    Added exit function if sign or submit give back error

--- sign.sh
+++ sign.sh
@@ -6,6 +6,7 @@ myExit() {
 	exit
 }
 
+
 PARENT="$(dirname $0)"
 if [[ ! -f "${PARENT}"/nft_env ]]; then
   echo -e "\nCommon env file missing: ${PARENT}/nft_env"
@@ -15,32 +16,42 @@ fi
 
 . "${PARENT}"/nft_env
 
+
+PARENT="$(dirname $0)"
+if [[ ! -f "${PARENT}"/nft_env ]]; then
+  echo -e "\nCommon env file missing: ${PARENT}/env"
+  echo -e "Please copy your env file to this Dir\n"
+  myExit 1
+fi
+
+. "${PARENT}"/env
+
+
+
+
 cd policy
-
-##### SIGN TEST#####
-
 
 
 ##### SIGN #####
 
 echo -e "\n\nSigning..." && sleep 3
 
-cardano-cli transaction sign \
+if ! cardano-cli transaction sign \
   --signing-key-file $CNODE_HOME/priv/wallet/nft/payment.skey \
   --signing-key-file policy.skey \
   --mainnet \
   --tx-body-file matx.raw \
-  --out-file matx.signed
-
+  --out-file matx.signed ; then sleep 1 && echo -e "\n\n${FG_RED}SIGN FAILED." && sleep 1 && echo -e "${NC}\n\nExiting Script ..." && sleep 1 && myExit; fi
 echo -e "${FG_LBLUE}Signed.${NC}" && sleep 0.4
 
 ##### SUBMIT #####
 
 echo -e "\nSubmitting..." && sleep 3
 
-cardano-cli transaction submit --tx-file  matx.signed --mainnet
+  if ! cardano-cli transaction submit --tx-file  matx.signed --mainnet ; then sleep 1 && echo -e "\n\n${FG_RED}SUBMIT FAILED.${NC}" && sleep 1 && echo -e "\n\nExiting Script ..." && sleep 1 && myExit; fi
+# cardano-cli transaction submit --tx-file  matx.signed --mainnet
 
-echo -e "${FG_LBLUE}Submited.${NC}" && sleep 1
+else echo -e "${FG_LBLUE}Submited.${NC}" && sleep 1
 
 ##### PRINTING INFO #####
 
